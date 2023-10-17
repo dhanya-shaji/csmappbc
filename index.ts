e6e78df8545a01483efed5301de342c10b339bea
abilashkrishnan <30994398+abilashkrishnan@users.noreply.github.com>
2023-10-17 15:39:49 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ var CategoryRouter = require("./src/routes/CategoryRouter");
 app.use("/product",ProductValidator, productRouter);
 app.use("/categories", CategoryRouter);
 
-const port = 80;
+const port = 8080;
 
 app.listen(port, () => {
     console.log(`API server listening on port ${port}`);
